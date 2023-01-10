1ba38c7f371c2d051f5a41ae41bb890a798cfbbc
Bartolomeu Aguiar <31780812+BartolomeuAguiar@users.noreply.github.com>
2023-01-10 18:45:44 -0400

    removed form crispy

--- delicierr/settings.py
+++ delicierr/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from django.contrib.messages import constants
 import os
 from pathlib import Path
+
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'orders',
     'products',
     'clients',
+    # THIRD APPS#
 ]
 
 MIDDLEWARE = [
